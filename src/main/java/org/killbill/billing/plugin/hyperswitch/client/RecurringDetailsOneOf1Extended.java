4a4ed18198733e3eb3e482657325fec67c54186f
Derrick Hammer <derrick@derrickhammer.com>
2025-01-21 22:46:06 -0500

    wip

--- src/main/java/org/killbill/billing/plugin/hyperswitch/client/RecurringDetailsOneOf1Extended.java
+++ src/main/java/org/killbill/billing/plugin/hyperswitch/client/RecurringDetailsOneOf1Extended.java
@@ -1,27 +1,31 @@
 package org.killbill.billing.plugin.hyperswitch.client;
 
 import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.hyperswitch.client.model.RecurringDetails;
 import com.hyperswitch.client.model.NetworkTransactionIdAndCardDetails;
 
-@JsonTypeName("payment_method_id")  // Changed this to match the expected variant
+@JsonTypeName("payment_method_id")
 public class RecurringDetailsOneOf1Extended extends RecurringDetails {
 
+    @JsonProperty("data")
     private String paymentMethodId;
 
     public RecurringDetailsOneOf1Extended setPaymentMethodId(String id) {
         this.paymentMethodId = id;
-        super.data(new NetworkTransactionIdAndCardDetails());
         return this;
     }
 
     @Override
+    @JsonProperty("data")
     public NetworkTransactionIdAndCardDetails getData() {
-        return new NetworkTransactionIdAndCardDetails() {
-            @Override
-            public String toString() {
-                return paymentMethodId;
-            }
-        };
+        // This still needs to exist for inheritance, but won't be used in serialization
+        return null;
+    }
+
+    // Add this to override the parent's serialization
+    @JsonProperty("data")
+    public String getStringData() {
+        return paymentMethodId;
     }
 }
