c10c1d130fcba60572ee9c8f9e8504074200f088
Derrick Hammer <derrick@derrickhammer.com>
2025-01-21 22:43:01 -0500

    wip

--- src/main/java/org/killbill/billing/plugin/hyperswitch/client/RecurringDetailsOneOf1Extended.java
+++ src/main/java/org/killbill/billing/plugin/hyperswitch/client/RecurringDetailsOneOf1Extended.java
@@ -4,21 +4,19 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.hyperswitch.client.model.RecurringDetails;
 import com.hyperswitch.client.model.NetworkTransactionIdAndCardDetails;
 
-@JsonTypeName("RecurringDetails_oneOf_1")
+@JsonTypeName("payment_method_id")  // Changed this to match the expected variant
 public class RecurringDetailsOneOf1Extended extends RecurringDetails {
 
     private String paymentMethodId;
 
     public RecurringDetailsOneOf1Extended setPaymentMethodId(String id) {
         this.paymentMethodId = id;
-        // Just create a dummy wrapper since we need something
         super.data(new NetworkTransactionIdAndCardDetails());
         return this;
     }
 
     @Override
     public NetworkTransactionIdAndCardDetails getData() {
-        // Override to return our payment method id instead
         return new NetworkTransactionIdAndCardDetails() {
             @Override
             public String toString() {
