f9c972bff26eb5de006ad771a35d5a290152e33f
Derrick Hammer <derrick@derrickhammer.com>
2025-01-21 22:54:53 -0500

    wip

--- src/main/java/org/killbill/billing/plugin/hyperswitch/client/RecurringDetailsOneOf1Extended.java
+++ src/main/java/org/killbill/billing/plugin/hyperswitch/client/RecurringDetailsOneOf1Extended.java
@@ -1,46 +1,30 @@
 package org.killbill.billing.plugin.hyperswitch.client;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.annotation.JsonValue;
 import com.hyperswitch.client.model.RecurringDetails;
 import com.hyperswitch.client.model.NetworkTransactionIdAndCardDetails;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import java.io.IOException;
 
 @JsonTypeName("payment_method_id")
-@JsonSerialize(using = RecurringDetailsOneOf1Extended.CustomSerializer.class)
 public class RecurringDetailsOneOf1Extended extends RecurringDetails {
 
-    private String paymentMethodId;
+    private static class StringOnlyData extends NetworkTransactionIdAndCardDetails {
+        private final String value;
+
+        StringOnlyData(String value) {
+            this.value = value;
+        }
+
+        // Override all serialization to just output the string
+        @JsonValue  // This tells Jackson to use this value directly
+        public String getValue() {
+            return value;
+        }
+    }
 
     public RecurringDetailsOneOf1Extended setPaymentMethodId(String id) {
-        this.paymentMethodId = id;
+        super.data(new StringOnlyData(id));
         return this;
-    }
-
-    @Override
-    public NetworkTransactionIdAndCardDetails getData() {
-        return null;
-    }
-
-    public String getPaymentMethodId() {
-        return paymentMethodId;
-    }
-
-    public static class CustomSerializer extends StdSerializer<RecurringDetailsOneOf1Extended> {
-        public CustomSerializer() {
-            super(RecurringDetailsOneOf1Extended.class);
-        }
-
-        @Override
-        public void serialize(RecurringDetailsOneOf1Extended value, JsonGenerator gen, SerializerProvider provider)
-            throws IOException {
-            gen.writeStartObject();
-            gen.writeStringField("type", "payment_method_id");
-            gen.writeStringField("data", value.getPaymentMethodId());
-            gen.writeEndObject();
-        }
     }
 }
